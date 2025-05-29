5105f44c5b14d53bf39069b871c7747a5b7550ef
AinhoaAlonso <ainhoaalonso88@gmail.com>
2025-05-29 09:27:00 +0200

    Cambios carpeta

--- Back/crud-clinica-dental/src/Users/dto/create-user.dto.ts
+++
@@ -1,8 +0,0 @@
-import { UserRole } from "../enums/rol.enum";
-
-export class CreateUserDto {
-  username_users: string;
-  password_users: string;
-  rol_users: UserRole;
-  is_active_users?: boolean; // Opcional
-}
